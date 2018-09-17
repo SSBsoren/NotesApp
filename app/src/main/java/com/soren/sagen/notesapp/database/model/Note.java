a0a9e908797a536d8111a42fecf5f91aad8bcd3e
saGen <sagensakam015@gmail.com>
2018-09-17 19:54:35 +0530

    Second Commit

--- app/src/main/java/com/soren/sagen/notesapp/database/model/Note.java
+++ app/src/main/java/com/soren/sagen/notesapp/database/model/Note.java
@@ -1,4 +1,58 @@
 package com.soren.sagen.notesapp.database.model;
 
-public class Note {
+public class Note
+{
+    public static final String TABLE_NAME = "notes";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOTE = "note";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+
+    private int  id;
+    private String note;
+    private String timestamp;
+
+    //Create table SQL query
+
+    public static final String CREATE_TABLE =
+            "CREATE TABLE" + TABLE_NAME + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COLUMN_NOTE + "TEXT ,"
+                    + COLUMN_TIMESTAMP +" DATETIME DEFAULT CURRENT_TIMESTAMP"
+                    +")";
+
+
+    public Note(){
+
+    }
+
+    public Note(int anInt, String string, String string1) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public static String getCreateTable() {
+        return CREATE_TABLE;
+    }
 }
